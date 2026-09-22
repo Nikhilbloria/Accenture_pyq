cbf377d98ef8e97c225cb9d7d65894b0d03c523e
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-09-22 22:06:20 +0530

    Time: 1 ms (45.32%), Space: 10.4 MB (52.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string> st;
-        string g = "";
-
-        for(char c : s) {
-            if(c != ' ') {
-                g += c;
-            }
-            else {
-                if(g != "") {
-                    st.push(g);
-                    g = "";
-                }
+        stack<string>st;
+        string  g= "";
+        for(char c:s){
+            if(c!=' '){
+                g+=c;
+            }else if(g!=""){
+                st.push(g);
+                g = "";
             }
         }
-
-        if(g != "") {
+        if(g!=""){
             st.push(g);
         }
-
         string ans = "";
-
-        while(!st.empty()) {
-            ans += st.top();
+        while(!st.empty()){
+            ans+=st.top();
             st.pop();
-
-            if(!st.empty()) {
-                ans += " ";
+            if(!st.empty()){
+                ans+=" ";
             }
         }
-
         return ans;
+
     }
 };
