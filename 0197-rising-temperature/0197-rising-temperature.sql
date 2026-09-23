9c78e79ea1faf61c096c6b8d446461b4ec5b6661
Nikhilbloria <149429337+Nikhilbloria@users.noreply.github.com>
2026-09-23 22:27:07 +0530

    Time: 552 ms (40%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -4,3 +4,7 @@ From Weather w1
 JOIN Weather w2
 where DATEDIFF(w1.recordDate,w2.recordDate) = 1
 ANd w1.temperature > w2.temperature;
+
+#it is like loop inside loop 
+#w1 row1 will be compared to w2 full rows
+#then w2 row2 will be compared to w2 full rows
